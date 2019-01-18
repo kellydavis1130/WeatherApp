13cc0f5c4c3a48eb5a71209c182ada0af3e454ca
Kelly A. Manning <kellydavis1130@hotmail.com>
2019-01-18 12:09:40 -0500

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,27 @@
+class WelcomeController < ApplicationController
+  def test
+    response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{params[zipcode]}&units=imperial&appid=#{ENV['openweather_api_key']}")
+    @location = response['name']
+    @temp = response['main']['temp']
+    @weather_icon = response['weather'][0]['icon']
+    @weather_words = response['weather'][0]['description']
+    @cloudiness = response['clouds']['all']
+    @windiness = response['wind']['speed']
+  end
+
+  def index
+    if params[:zipcode] != '' && !params[:zipcode].nil?
+      response = HHTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{params[zipcode]}&units=imperial&appid=#{ENV['openweather_api_key']}")
+      @status = response['cod']
+      #if no error returned from api all fill instance variables
+    if @status != '404' && response['message'] != 'city not found'
+      @location =response['name']
+      @temp = response['main']['temp']
+      @weather_icon = response['weather'][0]['icon']
+      @weather_words = response['weather'][0]['description']
+      @cloudiness = response['clouds']['all']
+      @windiness = response['wind']['speed']
+    end
+    end
+  end
+end

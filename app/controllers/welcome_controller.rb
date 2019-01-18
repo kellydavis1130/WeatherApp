ce4f842d3aff8af7fc1b443a82baa4cd934aa41d
Kelly A. Manning <kellydavis1130@hotmail.com>
2019-01-18 13:28:45 -0500

    WeatherApp

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,17 +10,21 @@ class WelcomeController < ApplicationController
   end
 
   def index
+      # checks that the state and city params are not empty before calling the API
     if params[:zipcode] != '' && !params[:zipcode].nil?
-      response = HHTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{params[zipcode]}&units=imperial&appid=#{ENV['openweather_api_key']}")
+      response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{params[:zipcode]}&units=imperial&appid=#{ENV['openweather_api_key']}")
       @status = response['cod']
-      #if no error returned from api all fill instance variables
+      # if no error is returned from the call, we fill our instance variables with the result of the call
     if @status != '404' && response['message'] != 'city not found'
-      @location =response['name']
+      @location = response['name']
       @temp = response['main']['temp']
       @weather_icon = response['weather'][0]['icon']
       @weather_words = response['weather'][0]['description']
       @cloudiness = response['clouds']['all']
       @windiness = response['wind']['speed']
+      else
+      # if there is an error, we report it to our user via the @error variable
+      @error = response['message']
     end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/test'
-  get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # the index page gets three routes:
+  root 'welcome#index'
+  get 'index' => 'welcome#index'
+  # "get" routes for when we initially come to the page,
+  # (whether typing in manually, or coming to as the root)
+  post 'index' => 'welcome#index'
+  # a "post route for when we come to the page" after submitting the form
+  get 'test' => 'welcome#test'
 end
